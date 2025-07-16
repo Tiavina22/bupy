33a700ef747cb672c1a61b22bb541d5711057915
Rohan29 <rohanrc101@gmail.com>
2025-07-17 01:11:31 +0400

    [feat] function to encrypt file

---
+++ src/utils/encrypt.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const crypto = require('crypto')
+const { error } = require('console')
+
+function encryptFile(filePath, outputPath, password) {
+    return new Promise((resolve, reject) => {
+
+        const key = crypto.createHash('sha256').update(password).digest();
+        const iv = crypto.randomBytes(16);
+
+
+        const cipher = crypto.createCipheriv("aes-256-cbc", key, iv)
+
+        const input = fs.createReadStream(filePath)
+        const output = fs.createWriteStream(outputPath)
+
+        input.pipe(cipher).pipe(output)
+
+        output.on('finish', () => {
+            resolve()
+        })
+
+        output.on('error', () => {
+            reject(error)
+        })
+    })
+}
+
+
+
+module.exports = {
+    encryptFile
+}
